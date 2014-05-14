2cbbf73733ca4583bf2b1d8b751b68f62c88f8db
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-05-14 19:24:52 -0400

    first release of gps

--- week_7/7_gps3.1/our_solution.rb
+++ week_7/7_gps3.1/our_solution.rb
@@ -1,34 +1,81 @@
 # U3.W7: Build an Electronic Grocery List!
  
 # Your full names:
-# 1.
-# 2.
+# 1. Kenneth Mendonca
+# 2. Amol Borcar
  
 # User Stories (As many as you want. Delete the statements you don't need)
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
+# As a user, I want to add item to grocery list with desired quantity
+# As a user, I want to delete items from the list
+# As a user, I want to check items off the list
+# As a user, I want to display the list with items purchased and not
  
 # Pseudocode
+# create new class for specific grocery item
+# initialize item with name, quantity, and if purchased
 # 
-# 
-# 
-# 
-# 
-# 
- 
+# create new class for the grocery list
+# create methods for adding, deleting, and displaying items
+
  
 # Your fabulous code goes here....
 
+class GroceryItem
 
+    def initialize(item_name, quantity, purchased = false)
+      @item_name = item_name
+      @quantity = quantity
+      @purchased = purchased
+    end
+    attr_accessor :item_name, :quantity, :purchased
+    
+end
 
-
+class GroceryList
+	attr_accessor :list
+    
+    def initialize()
+      @list = []
+    end
+    
+    def add_item(*item)
+      item.each { |item| list << item }
+    end
+    
+    def delete_item(*item)
+      item.each do |item|
+        list.delete(item)
+      end
+    end
+    
+    def check_off(*item)
+      item.each { |item| item.purchased = true }
+    end
+     
+    def view_list
+      p "The following items are in your list:"
+      list.each do |item|
+        if item.purchased
+          puts "[x] " + item.item_name + ": " + item.quantity.to_s
+        else
+         puts "[ ]" + item.item_name + ": " + item.quantity.to_s
+        end
+      end
+    end
+    
+end
 
 
 
 # DRIVER CODE GOES HERE. 
  
- 
+banana = GroceryItem.new("Banana", 5)
+apple = GroceryItem.new("Apple", 10)
+list = GroceryList.new()
+list.add_item(banana, apple)
+list.view_list
+list.check_off(banana)
+list.view_list
+
+
  
