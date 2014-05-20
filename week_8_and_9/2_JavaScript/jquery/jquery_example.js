4cc747437760b4a4fac2f08ec9d1301b44e4320f
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-05-20 15:46:48 -0700

    release 3

--- week_8_and_9/2_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/2_JavaScript/jquery/jquery_example.js
@@ -2,21 +2,51 @@ $(document).ready(function(){
  
 //RELEASE 0: 
   //Link this script and the jQuery library to the jquery_example.html file and analyze what this code does. 
- 
 $('body').css({'background-color': 'pink'})
+
  
 //RELEASE 1:
   //Add code here to select elements of the DOM 
- 
+bodyElement = $('body')
+headerOne = $('h1')
+mascot = $('.mascot')
+mascotImage = $('.mascot img')
+
  
 //RELEASE 2: 
   // Add code here to modify the css and html of DOM elements
- 
+ headerOne.css ({
+ 	'text-align':'center',
+ 	'color': 'blue', 
+ 	'border': '10px solid black', 
+ 	'visibility':''
+ })
+ mascot.css({'text-align' : 'center'})
+ mascotImage.css({'width':'50%'})
+
+
  
 //RELEASE 3: Event Listener
   // Add the code for the event listener here 
- 
- 
+  $('img').on('mouseover', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'http://www.listofimages.com/wallpapers/2012/07/lightsaber-fun-humor-sword-grass-woodchuck-animals-1024x1280.jpg')
+  })
+
+   $('img').on('mouseleave', function(e){
+     e.preventDefault()
+    $(this).attr('src', 'http://scm-l3.technorati.com/12/12/13/73853/devbootcamp.png?t=201212131312312')
+  })
+
+
+
+   
+// I don't understand what the function (e) is doing.
+// how would I change the properties of the images? i.e. width height  
+
+
+
+  	
 //RELEASE 4 : Experiment on your own
  
  
@@ -24,4 +54,6 @@ $('body').css({'background-color': 'pink'})
  
  
  
-})  // end of the document.ready function: do not remove or write DOM manipulation below this.
+})  // end of the document.ready function: do not remove or write DOM manipulation below this
+
+
