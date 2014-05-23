c0a37aba9363ab4554b4e793ffa161c2016b18ee
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-05-23 15:08:31 -0700

    Release 0

--- week_8_and_9/3_Ruby/oo_basics/my_solution.rb
+++ week_8_and_9/3_Ruby/oo_basics/my_solution.rb
@@ -12,11 +12,25 @@
 class Student
   attr_accessor :scores, :first_name
 
-  def initialize(args)   #Use named arguments!
-    #your code here
+  def initialize(first_name, scores)   #Use named arguments! 
+  	@scores = scores #Why is this line necessary for me to print out @scores when calling average if attr_accessor is already there?
+  	@first_name =first_name
   end
+
+  def average
+  end
+
 end
 
+
+
+Alex = Student.new("Alex", [100,100,100,0,100])
+Ken = Student.new("Ken", [100,100,100,95,110])
+Naomi = Student.new("Naomi", [100, 55, 75, 100, 95])
+Rolf = Student.new("Rolf", [75, 75, 75, 85, 100])
+Sophia = Student.new("Sophia", [69, 69, 96, 96, 100])
+
+students = [Alex, Ken, Naomi, Rolf, Sophia]
 
 # 4. Refactored Solution
 
