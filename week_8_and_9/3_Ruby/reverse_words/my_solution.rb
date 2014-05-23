efc7d7f7cae448b716964c6bcc9731b56f3d48b6
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-05-23 13:15:31 -0700

    release 0/1

--- week_8_and_9/3_Ruby/reverse_words/my_solution.rb
+++ week_8_and_9/3_Ruby/reverse_words/my_solution.rb
@@ -1,11 +1,16 @@
 # U3.W8-9: Reverse Words
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge my self.
 
 # 2. Pseudocode
+# define reverse_words method
+# input should be a sentence
+# output should be the same sentence, but each word reversed
 
-
+# reverse each word from sentence
+# append it to a list
+# output the list in the correct order
 
 # 3. Initial Solution
 
