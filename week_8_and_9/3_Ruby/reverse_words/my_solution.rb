08206e31e375222e1bf3bd393455e03461a089c8
Kenneth Mendonça <mendonca.kr@gmail.com>
2014-05-31 16:41:29 -0400

    Completed Reverse Words

--- week_8_and_9/3_Ruby/reverse_words/my_solution.rb
+++ week_8_and_9/3_Ruby/reverse_words/my_solution.rb
@@ -10,25 +10,40 @@
 
 # reverse each word from sentence
 # append it to a list
-# output the list in the correct order
+# join the list and print ou
 
 # 3. Initial Solution
 
+def reverse_words(sentence)
+	to_reverse = sentence.split()
+	reversed_string = []
 
+	to_reverse.each do |word|
+		reversed_string << word.reverse
+	end
+	return reversed_string.join(" ")
+
+end
 
 
 # 4. Refactored Solution
 
-
-
-
-
-
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
+# def random_word(length = 5)
+#   rand(36**length).to_s(36)
+# end
 
+# def assert
+#   raise "Assertion failed!" unless yield
+# end
 
+# word1 = random_word
+# word2 =random_word
 
-
+# puts assert {reverse_words(word1) == word1.reverse}
+# puts assert {reverse_words(word1 word2) == word1.reverse + word2.reverse}
 
 
 # 5. Reflection 
+# THis was unusually easy for a week 9 challenge. I found that this was a walk in the park compared to 
+# some of the other challenges in thsi section such as num_to_words .Regardless, it was nice to be able to  get back ino Ruby code.
